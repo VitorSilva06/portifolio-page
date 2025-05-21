e0ac9f53a534f9851f72ba08d03f3c56dd33864b
VitorVieira <164203021+VitorSilva06@users.noreply.github.com>
2025-05-20 22:17:55 -0300

    Estrutura v1 do portifolio com toda estilização e esqueleto criado, funcionalidade no botão btn-contato pelo app.js onde esconde o forms e aparece somente após pressionar o botão.

---
+++ js/app.js
@@ -0,0 +1,6 @@
+
+document.getElementById("btn-contato").addEventListener("click", function () {
+    const form = document.getElementById("form-container");
+    form.style.display = form.style.display === "none" ? "block" : "none";
+});
+
