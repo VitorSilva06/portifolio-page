c66eb6366232059f73564432790adddd50822866
VitorSilva <vitor.svieira4@senacsp.edu.br>
2025-06-04 21:48:48 -0300

    feat: alterar botão de contato para mudar imagem ao ativar/desativar

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
-
 document.getElementById("btn-contato").addEventListener("click", function () {
     const form = document.getElementById("form-container");
-    form.style.display = form.style.display === "none" ? "block" : "none";
-});
+    const img = document.getElementById("btn-img");
 
+    if (form.style.display === "none" || form.style.display === "") {
+        // Mostrar o formulário
+        form.style.display = "block";
+        img.src = "../portifolio-page/img/botao-seta-cima.png"; // seta para cima
+    } else {
+        // Esconder o formulário
+        form.style.display = "none";
+        img.src = "../portifolio-page/img/botao-seta-direita.png"; // seta para baixo
+    }
+});
